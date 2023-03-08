b625abac923f2cbed791712347b694343c057db2
yisus44 <jesusadrian1953.1@gmail.com>
2023-03-07 23:04:39 -0600

    added list of user instead of random numbers

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
+const container = document.getElementById("root");
 const light = document.querySelector(".light");
-const number = document.createElement("p");
-number.classList.add("number");
-light.appendChild(number);
 
-setInterval(() => {
-  const randomNumber = Math.floor(Math.random() * 10);
-  number.textContent = `Luz actual: ${randomNumber}`;
-  if (randomNumber < 3) {
-    light.classList.add("off");
-  } else {
-    light.classList.remove("off");
+const getData = async () => {
+  const url =
+    "https://i9y6lgqmg7.execute-api.us-east-2.amazonaws.com/dev/mesures";
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const data = await response.json();
+  return data;
+};
+
+const renderData = (data) => {
+  let totalHeight = 0; // initialize the total height to 0
+  light.innerHTML = "";
+  for (const element of data) {
+    const student = document.createElement("p");
+    const icon = document.createElement("i");
+    icon.classList.add("fas");
+    icon.classList.add("fa-lightbulb");
+    icon.classList.add("lightbulb");
+
+    const [firstMeasure] = element.mesures;
+    const firstMeasureValue = firstMeasure ? firstMeasure.mesure : 0;
+
+    if (firstMeasureValue < 50) {
+      icon.classList.add("off");
+    }
+    student.classList.add("card");
+    student.textContent = `${element.username} - ${
+      firstMeasure ? firstMeasure.mesure : "Sin registrar"
+    }`;
+
+    light.appendChild(student);
+    student.appendChild(icon);
+    totalHeight += student.offsetHeight; // add the height of each card element to the total height
+    console.log(element);
   }
-}, 3000);
+  light.style.height = `calc(60% + ${totalHeight}px)`; // adjust the height of the light element based on the total height of the card elements
+};
+async function start() {
+  try {
+    setInterval(async () => {
+      const data = await getData();
+      renderData(data);
+    }, 5000);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
